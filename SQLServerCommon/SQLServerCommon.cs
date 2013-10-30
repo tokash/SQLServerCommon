0ac628db53aa4cfa2a2e39459c132e2c210e453a
tokash <yossi.tokash@gmail.com>
2013-10-30 15:23:23 +0200

    Adding to GitHub

---
+++ SQLServerCommon/SQLServerCommon.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace SQLServerCommon
+{
+    public class SQLServerCommon
+    {
+        public static bool IsDatabaseExists(string iConnectionString, string iDatabaseName)
+        {
+            string sqlCreateDBQuery;
+            bool result = false;
+
+            try
+            {
+                //iConnectionString
+                //tmpConn = new SqlConnection("server=(local)\\SQLEXPRESS;Trusted_Connection=yes");
+
+                var tmpConn = new SqlConnection(iConnectionString);
+                sqlCreateDBQuery = string.Format("SELECT database_id FROM sys.databases WHERE Name = '{0}'", iDatabaseName);
+
+                using (tmpConn)
+                {
+                    using (SqlCommand sqlCmd = new SqlCommand(sqlCreateDBQuery, tmpConn))
+                    {
+                        tmpConn.Open();
+                        int databaseID = (int)sqlCmd.ExecuteScalar();    
+                        tmpConn.Close();
+
+                        result = (databaseID > 0);
+                    }
+                }
+            } 
+            catch (Exception ex)
+            { 
+                result = false;
+            }
+
+            return result;
+        }
+
+        public static void ExecuteNonQuery(string iSQLCommand, string iConnectionString)
+        {
+            SqlConnection sqlConn = new SqlConnection(iConnectionString);
+
+            SqlCommand myCommand = new SqlCommand(iSQLCommand, sqlConn);
+
+            try
+            {
+                sqlConn.Open();
+                myCommand.ExecuteNonQuery();
+            }   
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (sqlConn.State == ConnectionState.Open)
+                {
+                    sqlConn.Close();
+                }
+            }
+        }
+
+        public static void ExecuteQuery()
+        {
+
+        }
+
+        public static void CreateDatabase(string iDatabaseName, string iConnectionString)
+        {
+            String str;
+            SqlConnection myConn = new SqlConnection ("Server=localhost;Integrated security=SSPI;database=master");
+
+            str = "CREATE DATABASE MyDatabase ON PRIMARY " +
+                "(NAME = MyDatabase_Data, " +
+                "FILENAME = 'C:\\MyDatabaseData.mdf', " +
+                "SIZE = 2MB, MAXSIZE = 10MB, FILEGROWTH = 10%) " +
+                "LOG ON (NAME = MyDatabase_Log, " +
+                "FILENAME = 'C:\\MyDatabaseLog.ldf', " +
+                "SIZE = 1MB, " +
+                "MAXSIZE = 5MB, " +
+                "FILEGROWTH = 10%)";
+        }
+    }
+}
---
+++ SQLServerCommonTester/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SQLServerCommonTester
+{
+    class Program
+    {
+        private static readonly string connStringInitial =  "Server=TOKASHYOS-PC\\SQLEXPRESS;Integrated security=SSPI;database=master";
+        private static readonly string connString = "Server=TOKASHYOS-PC\\SQLEXPRESS;Integrated security=SSPI;database=drugs";
+
+        private static readonly string sqlCommandCreateDB = "CREATE DATABASE Drugs ON PRIMARY " +
+                "(NAME = Drugs, " +
+                "FILENAME = 'D:\\Drugs.mdf', " +
+                "SIZE = 2MB, MAXSIZE = 10MB, FILEGROWTH = 10%) " +
+                "LOG ON (NAME = Drugs_LOG, " +
+                "FILENAME = 'D:\\Drugs.ldf', " +
+                "SIZE = 1MB, " +
+                "MAXSIZE = 100MB, " +
+                "FILEGROWTH = 10%)";
+
+        private static readonly string drugsTableSchema = "CREATE TABLE Drugs (DrugID int IDENTITY(1,1), Name varchar(255) NOT NULL, PRIMARY KEY (DrugID))";
+        private static readonly string SideEffectsTableSchema = "CREATE TABLE SideEffects (SideEffectID int IDENTITY(1,1), Name varchar(255) NOT NULL, Description varchar(255), PRIMARY KEY (SideEffectID))";
+        private static readonly string ActiveIngredientsTableSchema = "CREATE TABLE ActiveIngredients (ActiveIngredientID int IDENTITY(1,1), Name varchar(255) NOT NULL, PRIMARY KEY (ActiveIngredientID))";
+        private static readonly string InactiveIngredientsTableSchema = "CREATE TABLE InactiveIngredients (InactiveIngredientID int IDENTITY(1,1), Name varchar(255) NOT NULL, PRIMARY KEY (InactiveIngredientID))";
+        private static readonly string Drugs_ActiveIngredientsTableSchema = "CREATE TABLE Drugs_ActiveIngredients (DrugID int FOREIGN KEY REFERENCES Drugs(DrugID), ActiveIngredientID int FOREIGN KEY REFERENCES ActiveIngredients(ActiveIngredientID))";
+        private static readonly string Drugs_InactiveIngredientsTableSchema = "CREATE TABLE Drugs_InactiveIngredients (DrugID int FOREIGN KEY REFERENCES Drugs(DrugID), InactiveIngredientID int FOREIGN KEY REFERENCES InactiveIngredients(InactiveIngredientID))";
+        private static readonly string Drugs_SideEffectsTableSchema = "CREATE TABLE Drugs_SideEffects (DrugID int FOREIGN KEY REFERENCES Drugs(DrugID), SideEffectID int FOREIGN KEY REFERENCES SideEffects(SideEffectID))";
+
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            //Create DB
+            if (!SQLServerCommon.SQLServerCommon.IsDatabaseExists(connStringInitial, "Drugs"))
+            {
+                SQLServerCommon.SQLServerCommon.ExecuteNonQuery(sqlCommandCreateDB, connStringInitial);
+
+                //Create tables upon DB creation
+                SQLServerCommon.SQLServerCommon.ExecuteNonQuery(drugsTableSchema, connString);
+                SQLServerCommon.SQLServerCommon.ExecuteNonQuery(SideEffectsTableSchema, connString);
+                SQLServerCommon.SQLServerCommon.ExecuteNonQuery(ActiveIngredientsTableSchema, connString);
+                SQLServerCommon.SQLServerCommon.ExecuteNonQuery(InactiveIngredientsTableSchema, connString);
+                SQLServerCommon.SQLServerCommon.ExecuteNonQuery(Drugs_ActiveIngredientsTableSchema, connString);
+                SQLServerCommon.SQLServerCommon.ExecuteNonQuery(Drugs_InactiveIngredientsTableSchema, connString);
+                SQLServerCommon.SQLServerCommon.ExecuteNonQuery(Drugs_SideEffectsTableSchema, connString);
+            }
+            stopWatch.Stop();
+
+            Console.WriteLine(String.Format("Time Elapsed for creating DB & tables: {0}", stopWatch.Elapsed));
+        }
+    }
+}
